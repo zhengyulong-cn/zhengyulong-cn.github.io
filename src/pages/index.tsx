6c7efc10515a714fed1ea4bb368acf04411bc09f
zhengyulong-cn <zhengyulong.cn@outlook.com>
2023-08-29 21:34:20 +0800

    fix: 修改主页Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/programing-skills">
+            to="/docs/front-programing-skills">
             开始
           </Link>
         </div>
